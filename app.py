e1885626504d7102c8e7a20f6eedda2851ac5e55
Sonu Kumar <sonuramashishnpm@gmail.com>
2026-01-24 20:50:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def ask():
         return jsonify({"response": f"Flask Error: {str(e)}"})
         
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
